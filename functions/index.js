67878bbc4389227a799fe6e0dde5a110ec9b868a
Asheer Rizvi <asheer.vil@gmail.com>
2020-02-22 21:04:34 +0530

    Added code to firebase functions for connecting to agent.

--- functions/index.js
+++ functions/index.js
@@ -1,8 +1,24 @@
 const functions = require('firebase-functions');
+const cors = require('cors')({ origin: true});
+const admin = require('firebase-admin');
+const serviceAccount = require('./service-account.json');
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//  response.send("Hello from Firebase!");
-// });
+admin.initializeApp({
+    credential: admin.credential.cert(serviceAccount),
+    databaseURL: 'https://pizza-bot-lqrjjy.firebaseio.com'
+});
+
+const { SessionsClient } = require('dialogflow');
+
+exports.dialogflowGateway = functions.https.onRequest((request, response) => {
+  cors(request, response, async () => {
+        const { queryInput, sessionId } = request.body;
+
+        const sessionClient = new SessionsClient({ credentials: serviceAccount  });
+        const session = sessionClient.sessionPath('pizza-bot-lqrjjy', sessionId);
+
+        const responses = await sessionClient.detectIntent({ session, queryInput});
+        const result = responses[0].queryResult;
+        response.send(result);
+    });
+});
