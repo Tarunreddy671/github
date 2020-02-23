845533dd49437d04bd32f4c0119af9ca92f487c4
asheerrizvi <asheer.vil@gmail.com>
2020-02-23 20:44:14 +0530

    Added fulfillment webhook for processing order submission and order query.

--- functions/index.js
+++ functions/index.js
@@ -29,24 +29,37 @@ const { WebhookClient } = require('dialogflow-fulfillment');
 
 exports.dialogflowWebhook = functions.https.onRequest(async (request, response) => {
     const agent = new WebhookClient({ request, response });
+    const db = admin.firestore();
 
     const result = request.body.queryResult;
 
     async function orderPizzaHandler(agent) {
-        const db = admin.firestore();
         const orderId = Math.random().toString(36).substr(2, 6);
         const order = db.collection('pizza.orders').doc(orderId);
 
-        let { date, time, size, topping } = result.parameters;
+        let { date, time, size, topping, name, address, phone } = result.parameters;
 
         date = moment(date).format('DD-MM-YYYY');
         time = moment(time).utcOffset("+05:30").format('h:mm A');
 
-        await order.set({ date, time, size, topping });
-        agent.add(`Order with Order ID: ${orderId} has been successfully placed for ${date} at ${time}. Pizza Size: ${size}, Pizza Topping: ${topping}`);
+        await order.set({ orderId, name, address, phone, date, time, size, topping });
+        agent.add(`Order with Order ID: ${orderId} has been successfully placed for ${date} at ${time}. Pizza Size: ${size.toUpperCase()}, Pizza Topping: ${topping.toUpperCase()}`);
+    }
+
+    async function orderStatusHandler(agent) {
+        const { orderId } = result.parameters;
+        const orderRef = db.collection('pizza.orders').doc(orderId);
+        try {
+            const orderDoc = await orderRef.get();
+            const order = orderDoc.data();
+            agent.add(`Name: ${order.name}, Pizza: ${order.size.toUpperCase()} ${order.topping.toUpperCase()}, Deliver: ${order.date} at ${order.time}`);
+        } catch(e) {
+            agent.add(`An error has occured: ${e}`);
+        }
     }
 
     let intentMap = new Map();
     intentMap.set('order.pizza', orderPizzaHandler);
+    intentMap.set('order.status', orderStatusHandler);
     agent.handleRequest(intentMap);
 });
